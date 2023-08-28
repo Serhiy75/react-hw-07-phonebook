14f8ec4b987df307500b05d8f84034ab58b431bd
sstfnk3107 <s.stefanyk75@gmail.com>
2023-08-28 21:25:42 +0300

    123

--- src/components/FormContacts/FormContacts.jsx
+++ src/components/FormContacts/FormContacts.jsx
@@ -1,43 +1,41 @@
-import { Component } from 'react';
+import React from 'react';
 import { nanoid } from 'nanoid';
 import PropTypes from 'prop-types';
 import css from './FormContact.module.css';
 
-export class FormContacts extends Component {
-  handleSubmit = evt => {
+export const FormContacts = ({ addContact }) => {
+  const handleSubmit = evt => {
     evt.preventDefault();
     const form = evt.currentTarget;
     const name = form.elements.name.value;
     const number = form.elements.number.value;
-    this.props.addContact({ number, name, id: nanoid() });
+    addContact({ number, name, id: nanoid() });
     form.reset();
   };
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit} className={css.forma}>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
-        <label>Number</label>
-        <input
-          type="tel"
-          name="number"
-          pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
-          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-          required
-        />
-        <button type="submit" className={css.formbtn}>
-          Add Contacts
-        </button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit} className={css.forma}>
+      <label>Name</label>
+      <input
+        type="text"
+        name="name"
+        pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+        required
+      />
+      <label>Number</label>
+      <input
+        type="tel"
+        name="number"
+        pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
+        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+        required
+      />
+      <button type="submit" className={css.formbtn}>
+        Add Contacts
+      </button>
+    </form>
+  );
+};
 
 FormContacts.propTypes = {
   addContact: PropTypes.func.isRequired,
